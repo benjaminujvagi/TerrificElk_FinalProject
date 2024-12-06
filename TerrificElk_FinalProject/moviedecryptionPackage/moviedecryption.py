eef74680c33d471835b8bfb2014dd437bbe937ff
Jacob Schulte <schul2jt@mail.uc.edu>
2024-12-06 14:02:20 -0500

    I added the movie decryption module

--- TerrificElk_FinalProject/moviedecryptionPackage/moviedecryption.py
+++ TerrificElk_FinalProject/moviedecryptionPackage/moviedecryption.py
@@ -1,1 +1,32 @@
 
+# moviedecryption/decryptor.py
+
+# Name: Ben Ujvagi, Jacob Shultze, Danny Barnhouse, Dobry Shaw
+# email:  ujvagibw@mail.uc.edu, schul2jt@mail.uc.edu, barnhodw@mail.uc.edu, shawdp@mail.uc.edu
+# Assignment Number: Final Project
+# Due Date:  12/10/2024
+# Course #/Section:  IS4010-001
+# Semester/Year:  Fall 2024
+# Brief Description of the assignment: decrypt two different codes and take a picture at the location
+# Brief Description of what this module does. In this module we decrypt the code that tells us our movie
+# Citations: Microsoft Copilot
+
+from cryptography.fernet import Fernet
+
+
+
+"""
+    Decrypts an encrypted message using a Fernet key.
+
+    Args:
+        encrypted_message (str): The message to decrypt.
+        key (bytes): The Fernet key for decryption.
+
+    Returns:
+        str: The decrypted message.
+"""
+
+def decrypt_message_fernet(encrypted_message, key):
+    fernet = Fernet(key)
+    decrypted_message = fernet.decrypt(encrypted_message.encode())
+    return decrypted_message.decode()
